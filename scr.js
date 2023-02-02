02581a8955f629aba361aa7f9eb4eb6a822ae54d
Naman <naman.khandelwal@intimetec.com>
2023-02-02 16:27:13 +0530

    After resolve all changes

--- scr.js
+++ scr.js
@@ -1,18 +1,20 @@
 const addBox = document.querySelector(".add-box");
 let popupBox = document.querySelector(".popup-box");
-let popupTitle = popupBox.querySelector("header p");
-let closeIcon = popupBox.querySelector("header i");
-let titleTag = popupBox.querySelector("input");
-let descTag = popupBox.querySelector("textarea");
+let popupTitle = document.querySelector("header p");
+let closeIcon = document.querySelector("header i");
+let titleTag = document.querySelector("input");
+let descTag = document.querySelector("textarea");
 let addBtn = document.querySelector(".button");
-
 let notes = JSON.parse(localStorage.getItem("notes"));
+let currentDate = new Date();
 let isUpdate = false;
+let noteInfo;
 let updateId;
 let filterDesc;
 let liTag;
 let description;
-let month_name = [
+let title;
+let monthName = [
   "Jan",
   "Feb",
   "March",
@@ -28,23 +30,15 @@ let month_name = [
 ];
 
 addBox.addEventListener("click", () => {
-  popupTitle.innerText = "Add a new Task";
-  addBtn.innerText = "Add Task";
-  popupBox.classList.add("show");
-  document.querySelector("body").style.overflow = "hidden";
-  if (window.innerWidth > 660) titleTag.focus();
+  openPopup();
 });
 
 closeIcon.addEventListener("click", () => {
-  isUpdate = false;
-  titleTag.value = descTag.value = "";
-  popupBox.classList.remove("show");
-  document.querySelector("body").style.overflow = "auto";
+  closePopup();
 });
 
 function showNotes() {
   if (!notes) return;
-
   document.querySelectorAll(".note").forEach((li) => li.remove());
   notes.forEach((note, id) => {
     filterDesc = note.description.replaceAll("\n", "<br/>");
@@ -58,7 +52,7 @@ function showNotes() {
                             <div class="settings">
                                 <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
                                 <ul class="menu">
-                                    <li class="edi" onclick="updateNote(${id}, '${note.title}', '${note.description}')"><i class="uil uil-pen"></i>Edit</li>
+                                    <li onclick="updateNote(${id}, '${note.title}', '${note.description}')"><i class="uil uil-pen"></i>Edit</li>
                                     <li class="del" onclick="deleteNote(${id})"><i class="uil uil-trash"></i>Delete</li>
                                 </ul>
                             </div>
@@ -68,56 +62,39 @@ function showNotes() {
   });
 }
 
-function showMenu(elem) {
-  elem.parentElement.classList.add("show");
+function showMenu(element) {
+  element.parentElement.classList.add("show");
   document.addEventListener("click", (event) => {
-    console.log(event.target.tagName);
-    if (event.target.tagName != "I" || event.target != elem) {
-      elem.parentElement.classList.remove("show");
+    if (event.target != elem) {
+      element.parentElement.classList.remove("show");
     }
   });
 }
 
 function deleteNote(noteId) {
-  document.querySelector(".menu").classList.remove("show");
-  notes.splice(noteId, 1);
-  localStorage.setItem("notes", JSON.stringify(notes));
+  remove(noteId);
   showNotes();
 }
 
-function note_change(noteId, title, filterDesc) {
-  document.querySelector(".menu").classList.remove("show");
-  description = filterDesc.replaceAll("<br/>", "\r\n");
-  updateId = noteId;
-  isUpdate = true;
-}
-
 function updateNote(noteId, title, filterDesc) {
-  note_change(noteId, title, filterDesc);
-  addBox.click();
-  titleTag.value = title;
-  descTag.value = description;
-  popupTitle.innerText = "Update Task";
-  addBtn.innerText = "Update Task";
+  modifyNote(noteId, title, filterDesc);
 }
 
 addBtn.addEventListener("click", (event) => {
   event.preventDefault();
-  let title = titleTag.value.trim(),
-    description = descTag.value.trim();
+  addNote(title, description);
+});
 
+function addNote(title, description) {
+  title = titleTag.value.trim();
+  description = descTag.value.trim();
   if (title && description) {
-    let currentDate = new Date(),
-      month = currentDate.getMonth(),
-      day = currentDate.getDate(),
-      year = currentDate.getFullYear();
-    hour = currentDate.getHours();
-    min = currentDate.getMinutes();
-
-    let noteInfo = {
+      noteInfo = {
       title,
       description,
-      date: ` ${month_name[month]}/${day}/${year} (${hour}:${min})`,
+      date: ` ${
+        monthName[currentDate.getMonth()]
+      }${currentDate.getDate()}/${currentDate.getFullYear()} (${currentDate.getHours()}:${currentDate.getMinutes()})`,
     };
     if (!isUpdate) {
       notes.push(noteInfo);
@@ -128,31 +105,41 @@ addBtn.addEventListener("click", (event) => {
     localStorage.setItem("notes", JSON.stringify(notes));
     showNotes();
     closeIcon.click();
+  } 
+  else {
+    alert("Please enter some valid Input");
   }
-});
+}
 
-(function showNotes() {
-  if (!notes) return;
+function openPopup() {
+  popupTitle.innerText = "Add a new Task";
+  addBtn.innerText = "Add Task";
+  popupBox.classList.add("show");
+  document.querySelector("body").style.overflow = "hidden";
+  if (window.innerWidth > 660) titleTag.focus();
+}
 
-  document.querySelectorAll(".note").forEach((li) => li.remove());
-  notes.forEach((note, id) => {
-    let filterDesc = note.description.replaceAll("\n", "<br/>");
-    let liTag = `<li class="note">
-                        <div class="details">
-                            <p>${note.title}</p>
-                            <span>${note.description}</span>
-                        </div>
-                        <div class="bottom-content">
-                            <span>${note.date}</span>
-                            <div class="settings">
-                                <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
-                                <ul class="menu">
-                                    <li class="edi" onclick="updateNote(${id}, '${note.title}', '${note.description}')"><i class="uil uil-pen"></i>Edit</li>
-                                    <li class="del" onclick="deleteNote(${id})"><i class="uil uil-trash"></i>Delete</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </li>`;
-    addBox.insertAdjacentHTML("afterend", liTag);
-  });
-})();
+function closePopup() {
+  isUpdate = false;
+  titleTag.value = descTag.value = "";
+  popupBox.classList.remove("show");
+  document.querySelector("body").style.overflow = "auto";
+}
+
+function modifyNote(noteId, title, filterDesc) {
+  document.querySelector(".menu").classList.remove("show");
+  description = filterDesc.replaceAll("<br/>", "\r\n");
+  updateId = noteId;
+  isUpdate = true;
+  addBox.click();
+  titleTag.value = title;
+  descTag.value = description;
+  popupTitle.innerText = "Update Task";
+  addBtn.innerText = "Update Task";
+}
+
+function remove(noteId) {
+  document.querySelector(".menu").classList.remove("show");
+  notes.splice(noteId, 1);
+  localStorage.setItem("notes", JSON.stringify(notes));
+}
