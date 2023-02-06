48859916bc94b140cb31d2e2cc99be5a0bee50f9
Naman <naman.khandelwal@intimetec.com>
2023-02-06 09:08:34 +0530

    Typescript Project complete

---
+++ script.js
@@ -0,0 +1,137 @@
+"use strict";
+const addBox = document.querySelector(".add-box");
+let popupBox = document.querySelector(".popup-box");
+let popupTitle = document.querySelector("header p");
+let closeIcon = document.querySelector("header i");
+let titleTag = document.querySelector("input");
+let descTag = document.querySelector("textarea");
+let addBtn = document.querySelector(".button");
+let body = document.querySelector("body");
+let notes = JSON.parse(localStorage.getItem("notes") || "[]");
+let closeButton = document.querySelector(".menu");
+let currentDate = new Date();
+let isUpdate = false;
+let noteInfo;
+let updateId;
+let filterDesc;
+let liTag;
+let description;
+let title;
+let monthName = [
+    "Jan",
+    "Feb",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+];
+addBox.addEventListener("click", () => {
+    openPopup();
+});
+closeIcon.addEventListener("click", () => {
+    closePopup();
+});
+function showNotes() {
+    if (!notes)
+        return;
+    document.querySelectorAll(".note").forEach((li) => li.remove());
+    notes.forEach((note, id) => {
+        filterDesc = note.description.replaceAll("\n", "<br/>");
+        liTag = `<li class="note">
+						  <div class="details">
+							  <p>${note.title}</p>
+							  <span>${note.description}</span>
+						  </div>
+						  <div class="bottom-content">
+							  <span>${note.date}</span>
+							  <div class="settings">
+								  <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
+								  <ul class="menu">
+									  <li onclick="updateNote(${id}, '${note.title}', '${note.description}')"><i class="uil uil-pen"></i>Edit</li>
+									  <li class="del" onclick="deleteNote(${id})"><i class="uil uil-trash"></i>Delete</li>
+								  </ul>
+							  </div>
+						  </div>
+					  </li>`;
+        addBox.insertAdjacentHTML("afterend", liTag);
+    });
+}
+function showMenu(element) {
+    element.parentElement.classList.add("show");
+    document.addEventListener("click", (event) => {
+        if (event.target != element) {
+            element.parentElement.classList.remove("show");
+        }
+    });
+}
+function deleteNote(noteId) {
+    remove(noteId);
+    showNotes();
+}
+function updateNote(noteId, title, filterDesc) {
+    modifyNote(noteId, title, filterDesc);
+}
+addBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    addNote(title, description);
+});
+function addNote(title, description) {
+    title = titleTag.value.trim();
+    description = descTag.value.trim();
+    if (title && description) {
+        noteInfo = {
+            title,
+            description,
+            date: ` ${monthName[currentDate.getMonth()]}/${currentDate.getDate()}/${currentDate.getFullYear()} (${currentDate.getHours()}:${currentDate.getMinutes()})`,
+        };
+        if (!isUpdate) {
+            notes.push(noteInfo);
+        }
+        else {
+            isUpdate = false;
+            notes[updateId] = noteInfo;
+        }
+        localStorage.setItem("notes", JSON.stringify(notes));
+        showNotes();
+        closeIcon.click();
+    }
+    else {
+        alert("Please enter some valid Input");
+    }
+}
+function openPopup() {
+    popupTitle.innerText = "Add a new Task";
+    addBtn.innerText = "Add Task";
+    popupBox.classList.add("show");
+    body.style.overflow = "hidden";
+    if (window.innerWidth > 660)
+        titleTag.focus();
+}
+function closePopup() {
+    isUpdate = false;
+    titleTag.value = descTag.value = "";
+    popupBox.classList.remove("show");
+    body.style.overflow = "auto";
+}
+function modifyNote(noteId, title, filterDesc) {
+    body.classList.remove("show");
+    description = filterDesc.replaceAll("<br/>", "\r\n");
+    updateId = noteId;
+    isUpdate = true;
+    addBox.click();
+    titleTag.value = title;
+    descTag.value = description;
+    popupTitle.innerText = "Update Task";
+    addBtn.innerText = "Update Task";
+}
+function remove(noteId) {
+    closeButton.classList.remove("show");
+    notes.splice(noteId, 1);
+    localStorage.setItem("notes", JSON.stringify(notes));
+}
---
+++ script.ts
@@ -0,0 +1,153 @@
+const addBox: HTMLElement = <HTMLElement>document.querySelector(".add-box");
+let popupBox: HTMLElement = <HTMLElement>document.querySelector(".popup-box");
+let popupTitle: HTMLElement = <HTMLElement>document.querySelector("header p");
+let closeIcon: HTMLElement = <HTMLElement>document.querySelector("header i");
+let titleTag: HTMLInputElement = <HTMLInputElement>(
+  document.querySelector("input")
+);
+let descTag: HTMLTextAreaElement = <HTMLTextAreaElement>(
+  document.querySelector("textarea")
+);
+let addBtn: HTMLButtonElement = <HTMLButtonElement>(
+  document.querySelector(".button")
+);
+let body: HTMLElement = <HTMLElement>document.querySelector("body");
+let notes: { title: string; description: string; date: string }[] = JSON.parse(
+  localStorage.getItem("notes") || "[]"
+);
+let closeButton: HTMLElement = <HTMLElement>document.querySelector(".menu");
+let currentDate: Date = new Date();
+let isUpdate: boolean = false;
+let noteInfo: { title: string; description: string; date: string };
+let updateId: number;
+let filterDesc: string;
+let liTag: string;
+let description: string;
+let title: string;
+let monthName = [
+  "Jan",
+  "Feb",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+addBox.addEventListener("click", () => {
+  openPopup();
+});
+
+closeIcon.addEventListener("click", () => {
+  closePopup();
+});
+
+function showNotes(): void {
+  if (!notes) return;
+  document.querySelectorAll(".note").forEach((li) => li.remove());
+  notes.forEach((note, id: Number) => {
+    filterDesc = note.description.replaceAll("\n", "<br/>");
+    liTag = `<li class="note">
+							<div class="details">
+								<p>${note.title}</p>
+								<span>${note.description}</span>
+							</div>
+							<div class="bottom-content">
+								<span>${note.date}</span>
+								<div class="settings">
+									<i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
+									<ul class="menu">
+										<li onclick="updateNote(${id}, '${note.title}', '${note.description}')"><i class="uil uil-pen"></i>Edit</li>
+										<li class="del" onclick="deleteNote(${id})"><i class="uil uil-trash"></i>Delete</li>
+									</ul>
+								</div>
+							</div>
+						</li>`;
+    addBox.insertAdjacentHTML("afterend", liTag);
+  });
+}
+function showMenu(element: any) {
+  element.parentElement.classList.add("show");
+  document.addEventListener("click", (event: Event) => {
+    if (event.target != element) {
+      element.parentElement.classList.remove("show");
+    }
+  });
+}
+
+function deleteNote(noteId: number) {
+  remove(noteId);
+  showNotes();
+}
+
+function updateNote(noteId: number, title: string, filterDesc: string): void {
+  modifyNote(noteId, title, filterDesc);
+}
+
+addBtn.addEventListener("click", (event): void => {
+  event.preventDefault();
+  addNote(title, description);
+});
+
+function addNote(title: string, description: string): void {
+  title = titleTag.value.trim();
+  description = descTag.value.trim();
+  if (title && description) {
+    noteInfo = {
+      title,
+      description,
+      date: ` ${
+        monthName[currentDate.getMonth()]
+      }/${currentDate.getDate()}/${currentDate.getFullYear()} (${currentDate.getHours()}:${currentDate.getMinutes()})`,
+    };
+    if (!isUpdate) {
+      notes.push(noteInfo);
+    } else {
+      isUpdate = false;
+      notes[updateId] = noteInfo;
+    }
+    localStorage.setItem("notes", JSON.stringify(notes));
+    showNotes();
+    closeIcon.click();
+  } else {
+    alert("Please enter some valid Input");
+  }
+}
+
+function openPopup(): void {
+  popupTitle.innerText = "Add a new Task";
+  addBtn.innerText = "Add Task";
+  popupBox.classList.add("show");
+  body.style.overflow = "hidden";
+  if (window.innerWidth > 660) titleTag.focus();
+}
+
+function closePopup(): void {
+  isUpdate = false;
+  titleTag.value = descTag.value = "";
+  popupBox.classList.remove("show");
+  body.style.overflow = "auto";
+}
+
+function modifyNote(noteId: number, title: string, filterDesc: string): void {
+  body.classList.remove("show");
+  description = filterDesc.replaceAll("<br/>", "\r\n");
+  updateId = noteId;
+  isUpdate = true;
+  addBox.click();
+  titleTag.value = title;
+  descTag.value = description;
+  popupTitle.innerText = "Update Task";
+  addBtn.innerText = "Update Task";
+}
+
+function remove(noteId: number): void {
+  closeButton.classList.remove("show");
+  notes.splice(noteId, 1);
+  localStorage.setItem("notes", JSON.stringify(notes));
+}
